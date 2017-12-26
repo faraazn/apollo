b54fe243afa3af30e3256ea431d8458fde75b491
Faraaz Nadeem <faraaz@Faraazs-MBP.home>
2017-12-26 02:01:33 -0500

    Fixed encoding for notes played at same time

--- xml_parser.py
+++ xml_parser.py
@@ -2,8 +2,8 @@ import os
 import music21
 import numpy as np
 
-corpus = ['/Users/faraaz/workspace/apollo/data/classical-musicxml/mozart/m15791_5.xml',
-			'/Users/faraaz/workspace/apollo/data/classical-musicxml/mozart/m5937_8.xml',
+corpus = ['/Users/faraaz/workspace/apollo/data/classical-musicxml/mozart/m5937_8.xml',
+			'/Users/faraaz/workspace/apollo/data/classical-musicxml/mozart/m15791_5.xml',
 			'/Users/faraaz/workspace/apollo/data/classical-musicxml/mozart/m15715_5.xml',
 			'/Users/faraaz/workspace/apollo/data/classical-musicxml/mozart/m15714_5.xml',
 			'/Users/faraaz/workspace/apollo/data/classical-musicxml/mozart/m17146_3.xml']
@@ -16,7 +16,7 @@ MIN_PITCH = MIN_NOTE.pitches[0].midi
 assert MAX_PITCH == 108
 assert MIN_PITCH == 21
 NOTE_RANGE = int(MAX_PITCH - MIN_PITCH + 1)
-TIME_SIGNATURE = music21.meter.TimeSignature('2/4')
+TIME_SIGNATURE = music21.meter.TimeSignature('3/4') # TODO: what happens when denominator!=4
 GRANULARITY = 16
 stats = {'discarded_less_measures': 0, 
 			'discarded_time_signature': 0, 
@@ -26,7 +26,7 @@ stats = {'discarded_less_measures': 0,
 			'total_cut_scores': 0,
 			'total_scores': 0,
 			'total_measures': 0}
-			
+
 def midi_to_note(midi_val):
 	octave = int((midi_val-12) / 12)
 	notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
@@ -37,7 +37,11 @@ assert midi_to_note(108) == 'C8'
 assert midi_to_note(21) == 'A0'
 
 # TODO: add check for number of beats actually in measure
-# TODO: decoded version seems off, bug appears to be in encoder
+# TODO: add key signature to encoding
+# TODO: add new vs continued note distinction in encoding
+# TODO: add support for pieces with pickups
+# TODO: what happens when time signature denominator != 4
+# TODO: remove dependency on midi_to_note function
 def encode_score(score):
 	assert len(score.parts) == 2 # discarded num parts
 	score = score.flattenParts()
@@ -50,37 +54,47 @@ def encode_score(score):
 			assert ts.ratioString == TIME_SIGNATURE.ratioString # discarded time signature
 			print(ts.ratioString)
 		
-		x = np.zeros((MEASURES_PER_CUT*GRANULARITY*TIME_SIGNATURE.beatCount, NOTE_RANGE))
+		x = np.zeros((int(MEASURES_PER_CUT*GRANULARITY*TIME_SIGNATURE.beatCount/4), NOTE_RANGE))
 		for measure in measures:
-			notes = measure.getElementsByClass('Note')
+			notes = measure.getElementsByClass(music21.note.Note)
+			chords = measure.getElementsByClass(music21.chord.Chord)
 			for note in notes:
 				if note.quarterLength < 4.0 / GRANULARITY:
 					continue # discarded high granularity
 				pitch = note.pitches[0].midi
 				if pitch < MIN_PITCH or pitch > MAX_PITCH:
 					continue # discarded pitch out of range
-				ind = measure.measureNumber % MEASURES_PER_CUT
-				ind *= int(GRANULARITY*TIME_SIGNATURE.beatCount)
-				ind += int(note.offset * GRANULARITY) # TODO: does offset work here?
+				ind = (measure.measureNumber-1) % MEASURES_PER_CUT
+				ind *= int(GRANULARITY*TIME_SIGNATURE.beatCount/4)
+				ind += int(note.offset * GRANULARITY/4) # TODO: does offset work here?
 				for i in range(int(note.quarterLength * GRANULARITY / 4)):
 					x[ind+i][pitch-MIN_PITCH] = 1
-		print(x[0])
-		print(x[1])
-		print(x[2])
+			for chord in chords:
+				if chord.quarterLength < 4.0 / GRANULARITY:
+					continue # discarded high granularity
+				for pitch in chord.pitches:
+					if pitch.midi < MIN_PITCH or pitch.midi > MAX_PITCH:
+						continue # discarded pitch out of range
+					ind = (measure.measureNumber-1) % MEASURES_PER_CUT
+					ind *= int(GRANULARITY*TIME_SIGNATURE.beatCount / 4)
+					ind += int(chord.offset * GRANULARITY/4) # TODO: does offset work here?
+					for i in range(int(chord.quarterLength * GRANULARITY / 4)):
+						x[ind+i][pitch.midi-MIN_PITCH] = 1
 		score_X.append(x)
 		start_ind += MEASURES_PER_CUT
 		cut_score = score.measures(start_ind, start_ind+MEASURES_PER_CUT-1)
 		measures = cut_score.getElementsByClass('Measure')
 	return score_X
-	
+
 def decode_score(encoding):
 	print(len(encoding))
-	assert len(encoding) == MEASURES_PER_CUT * GRANULARITY * TIME_SIGNATURE.beatCount
+	assert len(encoding) == MEASURES_PER_CUT * GRANULARITY * TIME_SIGNATURE.beatCount / 4
 	score = music21.stream.Stream()
+	score.timeSignature = TIME_SIGNATURE
 	measure_ind = 0
 	while measure_ind < MEASURES_PER_CUT:
-		start_beat = measure_ind * GRANULARITY * TIME_SIGNATURE.beatCount
-		end_beat = (measure_ind + 1) * GRANULARITY * TIME_SIGNATURE.beatCount
+		start_beat = int(measure_ind * GRANULARITY * TIME_SIGNATURE.beatCount / 4)
+		end_beat = int((measure_ind + 1) * GRANULARITY * TIME_SIGNATURE.beatCount / 4)
 		measure = music21.stream.Measure()
 		for beat_ind in range(start_beat, end_beat):
 			played_pitches = np.nonzero(encoding[beat_ind])[0]
@@ -109,4 +123,4 @@ for song_path in corpus:
 	except ZeroDivisionError:
 		pass
 
-decode_score(X[0]) # why is this length only 4?
+decode_score(X[0])
